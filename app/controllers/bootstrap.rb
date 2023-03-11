c719931a919ca4c617e0a3a9d51cffcee6611c10
alexrose1 <alex.rose1@kellogg.northwestern.edu>
2023-03-11 23:55:29 +0000

    Heroku edits

---
+++ app/controllers/bootstrap.rb
@@ -0,0 +1,7 @@
+class BootstrapController < ApplicationController
+
+    def index
+     
+    end
+
+end
---
+++ app/controllers/bootstrap_controller.rb
@@ -0,0 +1,6 @@
+class BootstrapController < ApplicationController
+
+
+   def  index 
+   end
+end
